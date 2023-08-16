6f262459c832e80bfd8d3c149b7baafd9e608fe2
liu980299 <liu980299@gmail.com>
2023-08-16 23:00:10 +1000

    fix issue for report for supporting jira tracking

--- messages.py
+++ messages.py
@@ -25,19 +25,19 @@ if __name__ == "__main__":
     server_url, job_name, username, password = args.jenkins.split("|")
     server = jenkins.Jenkins(server_url, username, password)
     job_info = server.get_job_info(job_name)
-    jobs = args.jobs.split("|")
+    job_args = args.jobs.split("|")
     teams = {}
     for team_str in args.teams.split(","):
         (env, webhook) = team_str.split("|", 1)
         teams[env] = pymsteams.connectorcard(webhook)
 
-    bookmarks = [job.split(":")[0].strip("#") for job in jobs if job.find("#") >= 0 ]
-    jobs = [job.split(":")[0].strip("#") for job in jobs]
-    reports = [job.split(":")[1] for job in jobs]
+    bookmarks = [job.split(":")[0].strip("#") for job in job_args if job.find("#") >= 0 ]
+    jobs = [job.split(":")[0].strip("#") for job in job_args]
+    reports = [job.split(":")[1] for job in job_args]
     job_reports = dict(zip(jobs,reports))
     jobs_build ={}
-    if "lastSuccessBuild" in job_info:
-        last_successful_task = job_info["lastSuccessBuild"]["url"] + "/messages/messages.json"
+    if "lastSuccessfulBuild" in job_info and job_info["lastSuccessfulBuild"]:
+        last_successful_task = job_info["lastSuccessfulBuild"]["url"] + "/messages/messages.json"
         response = server.jenkins_request(requests.Request('GET',last_successful_task))
         jobs_build = json.loads(response.text)
     res = {}
@@ -61,11 +61,11 @@ if __name__ == "__main__":
                                     teams[env].payload = messages_json[env]
                                     teams[env].send()
             else:
-                last_build = job_info["lastSuccessBuild"]["number"]
+                last_build = job_info["lastSuccessfulBuild"]["number"]
                 if last_build > str(build):
-                    message_json_url = job_info["lastSuccessBuild"]["url"] + "/" + job_reports[job_name] + "/messages.json"
+                    message_json_url = job_info["lastSuccessfulBuild"]["url"] + "/" + job_reports[job_name] + "/messages.json"
                     response = server.jenkins_request(requests.Request('GET', message_json_url))
-                    if response.status_code == "200":
+                    if response.status_code == 200:
                         if build_info["number"] > max_build:
                             max_build = build_info["number"]
                         messages_json = json.loads(response.text)
@@ -75,16 +75,23 @@ if __name__ == "__main__":
                                 teams_send(teams[env])
             res[job_name] = max_build
         else:
-            last_build = job_info["lastSuccessBuild"]["number"]
+            last_build = job_info["lastSuccessfulBuild"]["number"]
             res[job_name] = last_build
-            message_json_url = job_info["lastSuccessBuild"]["url"] + "/" + job_reports[job_name] + "/messages.json"
-            response = server.jenkins_request(requests.Request('GET', message_json_url))
-            if response.status_code == "200":
-                messages_json = json.loads(response.text)
-                for env in messages_json:
-                    if env in teams:
-                        teams[env].payload = messages_json[env]
-                        teams[env].send()
+            message_json_url = job_info["lastSuccessfulBuild"]["url"] + "/" + job_reports[job_name] + "/messages.json"
+            try:
+                response = server.jenkins_request(requests.Request('GET', message_json_url))
+                if response.status_code == 200:
+                    messages_json = json.loads(response.text)
+                    for env in messages_json:
+                        if env in teams:
+                            teams[env].payload = messages_json[env]
+                            teams[env].send()
+
+            except requests.exceptions.HTTPError as e:
+                print("no messages found for " + job_name)
+                pass
+            except jenkins.NotFoundException as e:
+                pass
 
     message_json = open("messages.json","w")
     json.dump(res,message_json,indent=4)
