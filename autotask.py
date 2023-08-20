d44caffac1d2926fd62d0253de73362b1f8f3e55
liu980299 <liu980299@gmail.com>
2023-08-20 21:48:40 +1000

    fix issue for report for supporting jira tracking

--- autotask.py
+++ autotask.py
@@ -233,9 +233,9 @@ if __name__ == "__main__":
                 version = ".".join(a_jira["version"].split(".")[:2])
                 if "updated" not in a_jira:
                     if "is_new" in a_jira and a_jira["is_new"]:
-                        issue = jira.create_issue(project=jira["project"], summary=jira["summary"],
-                                                      description=jira["description"], issuetype={'name': 'Bug'}, labels=['foundByAutomation'],
-                                                             fixVersions=[{"name":"Triage"}],customfield_12257=jira["steps"],
+                        issue = jira.create_issue(project=a_jira["project"], summary=a_jira["summary"],
+                                                      description=a_jira["description"], issuetype={'name': 'Bug'}, labels=['foundByAutomation'],
+                                                             fixVersions=[{"name":"Triage"}],customfield_12257=a_jira["steps"],
                                                              reporter={"name": user_data["username"]},assignee={"name":user_data["username"]})
                         print(issue)
                         a_jira["original_id"] = a_jira["id"]
